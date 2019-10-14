a9e42aee14264c7327d331212dd77f6da84660e7
mixnr1 <38935530+mixnr1@users.noreply.github.com>
2019-10-14 16:18:48 +0300

    Add files via upload

--- downloader.py
+++ downloader.py
@@ -1,7 +1,8 @@
 import time
 import requests
 from bs4 import BeautifulSoup
-from lxml import html
+import config
+import re
 
 start = time.time()
 start_tuple=time.localtime()
@@ -9,22 +10,25 @@ start_time = time.strftime("%Y-%m-%d %H:%M:%S", start_tuple)
 
 url='https://straume.lmt.lv/lv/video-saraksts/tutas-lietas'
 
-page=requests.get(url)
+page=requests.get(url, proxies=config.proxies, allow_redirects=False)
 soup = BeautifulSoup(page.content, 'html.parser')
 mydivs = soup.find_all("a", {"class": "video-title"})
-# print(len(mydivs))
-# print(mydivs[1])
-# print(type(mydivs[1]))
-# print(dir(mydivs[1]))
-# print(mydivs[1].attrs["href"])
-
 all_url=[]
 for i in range(0,len(mydivs)):
-    #  print(mydivs[i])#single video
      all_url.append('https://straume.lmt.lv'+mydivs[i].attrs["href"])
-print(all_url)
-
-    
+# all_mp4=[]
+file_text=open("links.txt",'w')
+for mp4 in all_url:
+    page=requests.get(mp4, proxies=config.proxies, allow_redirects=False)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    mymp4 = soup.find_all('script')
+    for script in mymp4:
+        if re.findall('/tvinet.lmt.lv/+.+mp4', str(script)):
+            # print(re.findall('/tvinet.lmt.lv/+.+mp4', str(script)))
+            # all_mp4.append(re.findall('/tvinet.lmt.lv/+.+mp4', str(script)))
+            file_text.write(str(re.findall('/tvinet.lmt.lv/+.+mp4', str(script))).split("'")[1] + '\n')
+file_text.close()
+   
 
 end = time.time()
 end_tuple = time.localtime()
